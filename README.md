2a74405119c3183448b56cd843cb0e6dc31051c5
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-04-18 13:02:28 -0500

    update

---
+++ README.md
@@ -0,0 +1,17 @@
+goal 
+
+create a working blackjack game 
+
+
+components
+
+ a deck with all the cards
+
+ a dealers hand 
+
+ a players hand 
+
+ a way to compare them 
+
+ a way to bet 
+
---
+++ index.js
@@ -0,0 +1,23 @@
+// game variables 
+const deck = [ 1,2,3,4,5,6,7,8,9,10]
+const totalBet = 0
+let playersHand = 0
+let dealersHand = 0
+let dealersMoney = 1000000000000000000000
+let playersMoney = 1000
+
+// buttons from html 
+const dealersTotal = document.getElementById("dealersTotal")
+const playersHand = document.getElementById("playersHand")
+
+
+
+
+
+
+
+
+// functions
+function Bet(bet) {
+let totalBet = totalBet + bet
+}
