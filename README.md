93b63a51b5cc661bc5fa528aedbf8ec47024b928
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-04-18 17:02:11 -0500

    test

--- README.md
+++ README.md
@@ -15,3 +15,41 @@ components
 
  a way to bet 
 
+
+
+only double on 9 10 or 11 if not a split user value 
+
+
+
+2 cards for dealer  (show one)  if dealer == 21 && player == not 21 player lose 
+
+2 cards 
+
+if user 9, 10 or 11 double 
+
+mathhing  offer plit 
+
+if user = 1 and splits hit only one time per card 
+
+on split !1 can be played normal 
+
+user total 
+
+bet total 
+
+
+
+
+
+bet() set bet 
+deal player 2 cards 
+deal dealer 2 cards 
+
+
+
+stand() 
+compare hands and decide winner 
+
+
+
+
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // game variables 
 const deck = [ 1,2,3,4,5,6,7,8,9,10]
 const totalBet = 0
-let playersHand = 0
-let dealersHand = 0
+let playersHandValue = 0
+let dealersHandValue = 0
 let dealersMoney = 1000000000000000000000
 let playersMoney = 1000
 
 // buttons from html 
-const dealersTotal = document.getElementById("dealersTotal")
-const playersHand = document.getElementById("playersHand")
+const name = document.getElementById()
+const name2= document.getElementById()
 
 
 
@@ -18,6 +18,43 @@ const playersHand = document.getElementById("playersHand")
 
 
 // functions
+
+
+
+
+
+
 function Bet(bet) {
 let totalBet = totalBet + bet
+}
+
+
+function getCards() {
+   
+        cardShuffle = Math.floor(Math.random() * 52) 
+
+}
+
+
+function StartGame() {
+
+    dealersHandValue == 2 cards
+    userHandValue == 2 cards
+
+    if (dealersHandValue === 21 && userHandValue !== 21 ) {
+        console.log("Dealer Wins")
+    }
+    else {
+        ContinueGame()
+    }
+}
+
+function ContinueGame() {
+    if (userHandValue < 21) {
+        console.log("dealer wins")
+    }
+}
+
+function hit() {
+    userHandValue = userHandValue + newCard
 }
