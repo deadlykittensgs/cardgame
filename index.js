faf283edd290f6b8952e6426c6cd384523508e62
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-04-21 19:13:25 -0500

    game needs a reset after win

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 let totalBet = 0
 let dealersMoney = 1000
 let playersMoney = 1000
+let userHandValue = 0
+let dealersHandValue = 0
 
 // buttons from html 
 const dealersTotal = document.getElementById("dealersTotal")
@@ -20,19 +22,14 @@ const playersMoneySlot = document.getElementById("playersMoney")
 
 
 // dynamic text
-// visableCard.innerText = `${getCards()}`
-// flippedCard.innerText = `${getCards()}`
-// playerCardOne.innerText = `${getCards()}`
-// playerCardTwo.innerText = `${getCards()}`
+
+
 //on click
 deal.addEventListener("click", () => dealGame()) // make deal cards
 betOne.addEventListener("click", () => bet(1)) // make add to bet
 betTwo.addEventListener("click", () => bet(10)) // make add to bet
-hitMe.addEventListener("click", hit) // gives player a card
-stand.addEventListener("click", () => standHit()) //runs the
-
-
-
+hitMe.addEventListener("click", () => hit(userHandValue)) // gives player a card
+stand.addEventListener("click", () => standHit(userHandValue,dealersHandValue)) //runs the
 
 
 // functions
@@ -45,31 +42,27 @@ function bet(bet) {
     totalBet = totalBet + bet
     betTotal.innerText = `Bet: ${totalBet}`
     playersMoneySlot.innerText = `${playersMoney}`
+    
+  
    
 }
+
+
 
 
 // bets are placed and the cards are dealt
 
 function dealGame() {
 console.log("deal game run")
-    betTotal.innerText = "IN GAME"
-    // dealers hand 
-    let dealersCardOne = getCards()
-    visableCard.innerText = `${dealersCardOne}`
-    let dealersCardTwo = getCards()
-    flippedCard.innerText = `${dealersCardTwo}`
-    let dealersHandValue = dealersCardOne + dealersCardTwo // dealer hand value
-    dealersTotal.innerText = `${dealersHandValue}`
-    
+betTotal.innerText = "IN GAME"
 
-   // users hand
-    let userCardOne = getCards()
-    playerCardOne.innerText = `${userCardOne}`
-    let userCardTwo = getCards()
-    playerCardTwo.innerText = `${userCardTwo}`
-    let userHandValue = userCardOne + userCardTwo  // user hand value
-    userTotal.innerText = `${userHandValue}` 
+
+ userHandValue = getUsersHand ()
+ dealersHandValue =getDealersHand()
+
+
+    console.log(userHandValue)
+    console.log(dealersHandValue)
 
     if (dealersHandValue === 21 && userHandValue !== 21 ) {
         dealersMoney = dealersMoney + (totalBet * 2)
@@ -77,23 +70,62 @@ console.log("deal game run")
         console.log("dealer Blackjack")
     }
 
+
+
+}
+
+function getUsersHand () {
+      // users hand
+      let userCardOne = getCards()
+      playerCardOne.innerText = `${userCardOne}`
+      let userCardTwo = getCards()
+      playerCardTwo.innerText = `${userCardTwo}`
+      let userHandValue = userCardOne + userCardTwo  // user hand value
+      userTotal.innerText = `${userHandValue}` 
+      return userHandValue
+}
+
+
+function getDealersHand() {
+        // dealers hand 
+        let dealersCardOne = getCards()
+        visableCard.innerText = `${dealersCardOne}`
+        let dealersCardTwo = getCards()
+        flippedCard.innerText = `${dealersCardTwo}`
+        let dealersHandValue = dealersCardOne + dealersCardTwo // dealer hand value
+        dealersTotal.innerText = `${dealersHandValue}`
+        return dealersHandValue
 }
 
 
 
-function standHit(dealershandvalueneeded) {
-    
-    if (dealersHandValue < userHandValue && dealersHandValue < 22 ) {
+
+
+function standHit(user, dealer) {
+
+    console.log("stand")
+    if (dealer < user && dealer < 22 ) {
     dealerNewCard = document.createElement('div')
-    newCard.class = "dealerHand"
-    cardForDealer = `${getCards()}`
-    dealerNewCard.innerText = cardForDealer
+    dealerNewCard.innerText = `${getCards()}`
+    dealersHandValue = dealersHandValue + parseInt(dealerNewCard.innerText) 
     computersHand.appendChild(dealerNewCard)
+    dealersTotal.innerText = `${dealersHandValue}` 
     }
 
+    if (dealersHandValue === userHandValue) {
+        push()
+    }
+
+    if (dealersHandValue >21) {
+     
+        PlayerWins()
+    }
+    if (dealersHandValue < 22 && dealersHandValue > userHandValue ) {
+    
+        dealerWins()
+
 }
-
-
+}
 
 function giveMoney(whoWon) {
     if (dealerWins) {
@@ -106,30 +138,45 @@ function giveMoney(whoWon) {
     }
 }
 
-function hit() {
+function hit(userHV) {
+    console.log("hit")
     newCard = document.createElement('div')
-    newCardRandom =`${getCards()}`
-    newCard.class = "userHand"
-    newCard.innerText = newCardRandom
+    newCard.innerText =`${getCards()}`
+    userHandValue = userHandValue + parseInt(newCard.innerText) 
     userHand.appendChild(newCard)
-
-    userTotal.innerText = `${userHandValue + newCardRandom}` 
-
-
-    if (userHandValue > 21) {
-        console.log('Dealer Wins')
+    userTotal.innerText = `${userHandValue}` 
+    if (userHandValue >21) {
+        dealerWins()
+       
     }
-    
 }
 
 
-// function ContinueGame() {
-//     if (userHandValue < 21) {
-//         console.log("dealer wins")
-//     }
-// }
+function dealerWins() {
+    console.log("Dealer Wins")
+    dealersMoney = dealersMoney + totalBet * 2
+    totalBet = 0
+
+    document.body.removeChild()
+
+    //needs to reset game
+
+}
+
+function PlayerWins() {
+    console.log("player Wins")
+    playersMoneyMoney = playersMoney + totalBet * 2
+    totalBet = 0
+
+    //needs to reset game
+
+}
+
+function push() {
 
 
+
+}
 
 // get a card 
 function getCards() {
@@ -137,18 +184,3 @@ function getCards() {
      return cardShuffle
  
  }
-
-
-
-
-//  if (dealersHandValue >22 && dealersHandValue > userHandValue) {
-//     Stand(totalBet, dealerWins)
-// }
-
-// if (userHandValue >22 && userHandValue > dealersHandValue) {
-//     Stand(totalBet, userWins)
-// }
-// if (userHandValue === dealersHandValue ) {
-//     console.log("push")
-// }
-// }
