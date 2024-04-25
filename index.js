5c5eb689772e5ec963daacf738aa83259ac88844
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-04-24 22:37:42 -0500

    comments added to clean up code

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
- // Function to save the value to localStorage
+ // Function to save a value of to localStorage
  function saveToLocalStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
   }
   
-  // Function to retrieve the value from localStorage
+  // Function to retrieve a value from localStorage
   function getFromLocalStorage(key) {
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : null;
   }
 
-// function
-let money = getFromLocalStorage('money') || 1000; 
+// function called to add players money to local storage 
+let money = getFromLocalStorage('money') || 1000; // Assign players money to the saved value or if no value is saved 1000
 saveToLocalStorage('money', money); // Save the updated counter to localStorage
 
 
@@ -21,14 +21,13 @@ saveToLocalStorage('money', money); // Save the updated counter to localStorage
 
 // game variables 
 let totalBet = 0
-let dealersMoney = 1000
+let dealersMoney = Infinity
 let playersMoney = money
 let userHandValue = 0
 let dealersHandValue = 0
 let dealersCardTwo = 0
 
 // buttons from html 
-
 const dealersTotal = document.getElementById("dealersTotal")
 const userTotal = document.getElementById("usersValueHolder")
 const deal = document.getElementById("deal")
@@ -45,12 +44,7 @@ const whoWon = document.getElementById("whoWon")
 const nextHandClick = document.getElementById("nextHand")
 const newGame = document.getElementById("newGame")
 
-
-playersMoneySlot.innerText = `${playersMoney}`
-
-
-
-//on click
+//on click listeners 
 deal.addEventListener("click", () => dealGame()) // make deal cards
 betOne.addEventListener("click", () => bet(1)) // make add to bet
 betTwo.addEventListener("click", () => bet(10)) // make add to bet
@@ -58,13 +52,20 @@ hitMe.addEventListener("click", () => hit(userHandValue)) // gives player a card
 stand.addEventListener("click", () => standHit(userHandValue,dealersHandValue)) //runs the
 newGame.addEventListener("click", ()=> newGameFunction()) // clear local storage
 
+
+
+
+// assign the players money value on load in 
+playersMoneySlot.innerText = `${playersMoney}`
+
+
 // functions
 
 // step 1: game starts empty page 
 
 
 
-// Step 2: bet amount is decided
+// Step 2: bet amount is decided and stored in local storage 
 function bet(bet) {
     
     playersMoney = playersMoney - bet
@@ -86,29 +87,22 @@ console.log("deal game run")
  userHandValue = getUsersHand()
  dealersHandValue = getDealersHand()
 
-    //console.log(userHandValue)
-    //console.log(dealersHandValue)
     if (dealersHandValue === 21 && userHandValue !== 21 ) {
 
+        // if dealer blackjack show the dealers hand
         dealersTotal.innerText = `${dealersHandValue}`
         flippedCard.innerText = `${dealersCardTwo}`
-        
-        console.log("dealer Blackjack") 
 
         whoWon.innerText = "Dealer Blackjack:  "
         nextHand = document.createElement('button')
         nextHand.innerText = "Next Hand"
         nextHand.id= 'nextHand'
         whoWon.appendChild(nextHand)
+        // click next hand to reset the game 
         nextHand.addEventListener("click", () => unplug())
-        
-       
     }
-
-
-
 }
-
+//This function gets the users hand
 function getUsersHand () {
       // users hand
       let userCardOne = getCards()
@@ -120,7 +114,7 @@ function getUsersHand () {
       return userHandValue
 }
 
-
+// This function gets the dealers hand (but does not show the second card)
 function getDealersHand() {
         // dealers hand 
         let dealersCardOne = getCards()
@@ -136,10 +130,9 @@ function getDealersHand() {
 
 
 
-function standHit(user, dealer) {
-
-    console.log("stand")
+function standHit(user) {
     
+    // this loop runs until the dealer wins or loses drawing the dealer cards (needs a stand on 17 added)
     while (dealersHandValue < user && dealersHandValue < 22 ) {
     dealerNewCard = document.createElement('div')
     dealerNewCard.innerText = `${getCards()}`
@@ -147,10 +140,9 @@ function standHit(user, dealer) {
     computersHand.appendChild(dealerNewCard)
     dealersTotal.innerText = `${dealersHandValue}` 
     }
-   
+   // after the loop is finished these if statements run a function to handle the winner
     if (dealersHandValue === userHandValue) {
         push()
-        
     }
 
     if (dealersHandValue >21) {
@@ -165,7 +157,7 @@ function standHit(user, dealer) {
 
 }
 
-
+// Once a dealer clicks the hit button this function gives them another card
 function hit() {
     console.log("hit")
     newCard = document.createElement('div')
@@ -179,7 +171,7 @@ function hit() {
     }
 }
 
-
+// What happens if the dealer wins
 function dealerWins() {
     console.log("Dealer Wins")
 
@@ -196,7 +188,7 @@ function dealerWins() {
 
 
 }
-
+// What happens if the player wins
 function PlayerWins() {
 
     dealersTotal.innerText = `${dealersHandValue}`
@@ -216,6 +208,8 @@ function PlayerWins() {
 
 }
 
+
+// This function tells what happens if the player and user tie 
 function push() {
 // needs game reset with bet pushed
 whoWon.innerText = "Push"
@@ -230,6 +224,8 @@ nextHand.addEventListener("click", () => unplug())
 
 
 function resetGame(winner) {
+
+    // If player wins
 if (winner === "player") {
     playersMoney = playersMoney + totalBet * 2
     money = playersMoney
@@ -237,29 +233,29 @@ if (winner === "player") {
     totalBet = 0
     console.log("playerGotMoney") 
 } 
+//if dealer wins 
 if (winner === "dealer") {
     dealersMoney = dealersMoney + totalBet * 2
     totalBet = 0
     console.log("dealerGotMoney")
     
 }
-
+// if tie 
  
 }
-
-
+// This function refreshes the page to remove all dynamical created divs
 function unplug() {
     location.reload()
 }
 
-// get a card 
+// This function gets a "card"
 function getCards() {
     let cardShuffle = Math.floor(Math.random() * 11) +1  
      return cardShuffle
  
  }
 
-
+// This function will refresh the local storage resetting user money if the player goes broke
  function newGameFunction() {
     console.log("newGame")
     playersMoney = 1000
